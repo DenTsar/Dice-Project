66771b72ac59349de9f272b50ed5c8ed5cbd6c95
akash10014285 <jainjakash@gmail.com>
2018-12-05 10:46:31 -0500

    Edited main file.
    None.

---
+++ DiceHolder.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+public class DiceHolder{
+
+	private ArrayList<Die> container;
+
+	public DiceHolder(){
+		container = new ArrayList<>();
+	}
+
+	public int addDie(Die die){
+		if (container.size()<6){
+			container.add(die);
+			return 1;
+		}
+		else
+			return -1;
+	}
+
+	public void shake(){
+		for (int i = 0 ; i<container.size(); i++){
+			container.get(i).roll();
+		}
+	}
+
+	public String toString(){
+		String output = "";
+		for (int i = 0 ; i<container.size(); i++)
+			output+= container.get(i)+"\n";
+
+		return output;
+	}
+
+}
