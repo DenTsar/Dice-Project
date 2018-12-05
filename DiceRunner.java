f912f594e133e0fb41ff9fc2baf5d538673f326d
DenTsar <45399002+DenTsar@users.noreply.github.com>
2018-12-05 10:17:43 -0500

    Die updated

--- DiceRunner.java
+++ DiceRunner.java
@@ -1,2 +1,17 @@
 public class DiceRunner{
+	public static void main(String[]args){
+		//Test 1
+		Die a = new Die();
+		Die b = new Die(3);
+		int count = 0;
+		for(;a.getValue()!=1 || b.getValue()!=1; count++){
+			a.roll();
+			b.roll();
+			System.out.println("Roll "+(count+1));
+			System.out.println(a);
+			System.out.println(b);
+		}
+		System.out.println("It took "+count+" number of rolls to get snake eyes.");
+		//Test 2
+	}
 }
--- Die.java
+++ Die.java
@@ -3,17 +3,17 @@ public class Die{
 	private int value;
 	public Die(){
 		number = 6;
-		value = (int)(Math.random()*value)+1;
+		value = (int)(Math.random()*number)+1;
 	}
 	public Die(int number){
 		this.number = number;
-		value = (int)(Math.random()*value)+1;
+		value = (int)(Math.random()*number)+1;
 	}
 	public int getValue(){
 		return value;
 	}
 	public void roll(){
-		value = (int)(Math.random()*value)+1;
+		value = (int)(Math.random()*number)+1;
 	}
 	public String toString(){
 		return "Number of sides: "+number+", Value: "+value;
